002949b2508322f258c3516aa5c9824a18897fb6
eivitskiy <eivitskiy@gmail.com>
2019-02-12 17:00:26 +0700

    NEW: - added 404 page

--- core/App.php
+++ core/App.php
@@ -25,7 +25,11 @@ class App
         try {
             self::$kernel->launch();
         } catch (\Exception $e) {
-            //
+//            exit($e->getMessage());
+
+            header("HTTP/1.0 404 Not Found");
+            echo file_get_contents(APP_PATH . 'views/404.html');
+            exit();
         }
     }
 }
